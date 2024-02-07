e9c3dc79008ff225383f2f94a67fc020f8c4341c
Michael <mikembugua960@gmail.com>
2024-02-07 21:28:23 +0300

    minor changes

--- app.js
+++ app.js
@@ -5,10 +5,21 @@ const Payment = require("./models/Payment");
 const port = 5000;
 const mongoose = require("mongoose");
 const db = process.env.DATABASE_URL;
+app.use(express.json());
 mongoose
   .connect(db)
   .then(() => console.log("database connected"))
   .catch((err) => console.log(err));
+
+app.post("/payments", async (req, res) => {
+  try {
+    const payment = await Payment.create(req.body);
+    return res.status(201).json({ msg: "Created succesfful", payment });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+});
 
 app.get("/pay", (req, res) => {
   res.status(200).json({ message: "You are about to initiate the payment" });
